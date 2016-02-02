3f038d0dff44ceaf8811a4ded7bfabb443788de2
bessonov87 <bessonov87@gmail.com>
2016-02-02 17:04:32 +0200

    02022016

--- frontend/views/post/comment_add.php
+++ frontend/views/post/comment_add.php
@@ -22,7 +22,6 @@ if(!Yii::$app->user->isGuest):
 
         <div class="form-line comm-text">
             <?= $form->field($model, 'text')->textarea()->label('Текст сообщения') ?>
-            <?php var_dump(Yii::$app->params['comments']); ?>
             <span>Минимум: <?= Yii::$app->params['comments']['min_length'] ?> символов, Максимум: <?= Yii::$app->params['comments']['max_length'] ?> символов</span>
         </div>
         <div class="form-line">
@@ -62,41 +61,3 @@ endif;
         При добавлении комментария произошла ошибка.
     </div>
 <?php endif; ?>
-
-
-<div style="display: none;">
-    <div id="comment-reply">
-        [isLogged]
-        <h2>Ответ на комментарий #<span id="comm-id-span"></span></h2>
-        <h3>Пользователя: <span id="comm-autor-span"></span></h3>
-        <form method="post" id="reply-form">
-            <input type="hidden" name="reply_id" id="reply-hidden" value="none">
-            <input type="hidden" name="fcode" value="{fcode}">
-            <input type="hidden" name="fid" value="{fid}">
-            <div class="form-line">
-                <textarea name="text" placeholder="Текст сообщения..." required="required"></textarea>
-                <span>Минимум: <?= Yii::$app->params['comments']['min_lenght'] ?> символов, Максимум: {comm_max} символов</span>
-            </div>
-            <div class="form-line">
-                <div class="form-buttons">
-                    <button type="submit" name="submit_comment" value="register">
-                        <i class="fa fa-arrow-circle-o-right"></i> Отправить
-                    </button>
-                    <button type="button" onclick="$.fancybox.close();">
-                        <i class="fa fa-ban"></i> Отмена
-                    </button>
-                    <button type="button" onclick="clearForm('inline_login');" name="clear_form">
-                        <i class="fa fa-times"></i> Очистить
-                    </button>
-                </div>
-            </div>
-        </form>
-        [/isLogged]
-        [notLogged]
-        <div class="not-logged-message">
-            <span>Для того, чтобы ответить на комментарий, вам необходимо <i class="fa fa-external-link"></i> <a href="#inline_login" class="fancybox">авторизоваться</a>.</span>
-            <span>Если у вас еще нет учетной записи на нашем сайте, предлагаем <i class="fa fa-external-link"></i> <a href="#inline_register" class="fancybox">зарегистрироваться</a>.<br>Это займет не более 5 минут.</span>
-        </div>
-        [/notLogged]
-    </div>
-</div>
