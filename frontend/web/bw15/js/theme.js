0e519a46df06816513ba783ebab235ef826d7ca9
bessonov87 <bessonov87@gmail.com>
2016-03-26 15:49:42 +0200

    fixed tizerlady floating block

--- frontend/web/bw15/js/theme.js
+++ frontend/web/bw15/js/theme.js
@@ -177,4 +177,11 @@ $(document).ready(function() {
         });
     });
 
+    var topPos = $('.floating').offset().top; //topPos - это значение от верха блока до окна браузера
+    $(window).scroll(function() {
+        var top = $(document).scrollTop();
+        if (top > topPos) $('.floating').addClass('fixed');
+        else $('.floating').removeClass('fixed');
+    });
+
 });
