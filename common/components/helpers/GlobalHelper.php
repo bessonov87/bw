a97381f9583af98f8c9a56961d192ea0ff9f4a1f
Sergey Bessonov <bessonov87@gmail.com>
2016-10-22 10:58:51 +0300

    fixed date

--- common/components/helpers/GlobalHelper.php
+++ common/components/helpers/GlobalHelper.php
@@ -471,16 +471,16 @@ class GlobalHelper
         $summary['postsYesterday'] = Post::find()->where(['between', 'date', $yesterday_begin, $yesterday_end])->count();
         // Пользователи
         $summary['usersCount'] = User::find()->count();
-        $summary['usersToday'] = User::find()->where(['between', 'created_at', strtotime($today_begin), strtotime($today_end)])->count();
-        $summary['usersYesterday'] = User::find()->where(['between', 'created_at', strtotime($yesterday_begin), strtotime($yesterday_end)])->count();
+        $summary['usersToday'] = User::find()->where(['between', 'created_at', $today_begin, $today_end])->count();
+        $summary['usersYesterday'] = User::find()->where(['between', 'created_at', $yesterday_begin, $yesterday_end])->count();
         // Комментарии
         $summary['commentsCount'] = Comment::find()->count();
         $summary['commentsToday'] = Comment::find()->where(['between', 'date', $today_begin, $today_end])->count();
         $summary['commentsYesterday'] = Comment::find()->where(['between', 'date', $yesterday_begin, $yesterday_end])->count();
         // Ошибки
         $summary['errorsCount'] = Log::find()->count();
-        $summary['errorsToday'] = Log::find()->where(['between', 'log_time', strtotime($today_begin), strtotime($today_end)])->count();
-        $summary['errorsYesterday'] = Log::find()->where(['between', 'log_time', strtotime($yesterday_begin), strtotime($yesterday_end)])->count();
+        $summary['errorsToday'] = Log::find()->where(['between', 'log_time', $today_begin, $today_end])->count();
+        $summary['errorsYesterday'] = Log::find()->where(['between', 'log_time', $yesterday_begin, $yesterday_end])->count();
 
         return $summary;
     }
