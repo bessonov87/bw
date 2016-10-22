74689645c89737a7ede090933f5cd7fe0f2ed8a8
Sergey Bessonov <bessonov87@gmail.com>
2016-10-22 13:49:26 +0300

    fixed redirect

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -3,7 +3,7 @@
 /* Rewrite страниц категорий без слэша на конце на страницы со слэшом */
 $uri = $_SERVER['REQUEST_URI'];
 $pattern0 = '@^.*/$@si';
-$pattern1 = '/^.*\.(html|xml|gif|png|jpg)$/si';
+$pattern1 = '/\.(html|xml|gif|png|jpg|css|js)/si';
 $pattern2 = '/^\/(site|gii|ajax|debug)\/.*$/si';
 
 if(!preg_match($pattern0, $uri) && !preg_match($pattern1, $uri) && !preg_match($pattern2, $uri)){
