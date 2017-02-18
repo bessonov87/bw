a8b1660c8792a47c8296d8a8295987c07b51218a
Sergey Bessonov <bessonov87@gmail.com>
2017-02-18 12:27:16 +0200

    changed oauth config

--- common/config/main.php
+++ common/config/main.php
@@ -11,45 +11,6 @@ return [
             'showScriptName' => false,
             'enableStrictParsing' => false,
         ],
-        'authClientCollection' => [
-            'class' => 'yii\authclient\Collection',
-            'clients' => [
-                'google' => [
-                    'class' => 'yii\authclient\clients\Google',
-                    'clientId' => '616365204145-8n22kcvmq7lvi9ldsahdsq1vh65g8l0b.apps.googleusercontent.com',
-                    'clientSecret' => 'amtIr4rLG0gDkOEGp3eHEJkJ',
-                ],
-                'facebook' => [
-                    'class' => 'yii\authclient\clients\Facebook',
-                    'clientId' => '980427198678106',
-                    'clientSecret' => '32c93df394004bbce4684fcaf584f524',
-                ],
-                'vkontakte' => [
-                    'class' => 'yii\authclient\clients\VKontakte',
-                    'clientId' => '5321829',
-                    'clientSecret' => 'ZpCs0DRICDe9xJpArFOp',
-                    'scope' => 'email',
-                ],
-                /*'odnoklassniki' => [
-                    'class' => 'frontend\components\auth\Odnoklassniki',
-                    'applicationKey' => 'CBANKJELEBABABABA',
-                    'clientId' => '1246276608',
-                    'clientSecret' => '5D187AE7585B556E04E28BDA',
-                    'scope' => 'email',
-                ],*/
-                'yandex' => [
-                    'class' => 'yii\authclient\clients\Yandex',
-                    'clientId' => '2de9eb2993f341eea212f95b71b11429',
-                    'clientSecret' => 'd23cc79f700a49bc891c1c621bee1f5c',
-                ],
-                'mailru' => [
-                    'class' => 'frontend\components\auth\Mailru',
-                    'clientId' => '742178',
-                    'clientSecret' => '18d8ee2f97a100edeee850456577ef30',
-                ],
-                // и т.д.
-            ],
-        ],
         'elasticsearch' => [
             'class' => 'yii\elasticsearch\Connection',
             'nodes' => [
